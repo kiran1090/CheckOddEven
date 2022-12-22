2930489f8f78c9beb5d57c753611e9f5c7a6de6f
kiran1090 <63589265+kiran1090@users.noreply.github.com>
2022-12-22 15:50:58 +0530

    Add files via upload

---
+++ src/CheckOddEven.java
@@ -0,0 +1,14 @@
+public class CheckOddEven {
+    public static void main(String[] arg) {
+        int n = 31;
+if(n%2==0)
+{
+    System.out.println("The number is Even");
+
+}
+else
+{
+    System.out.println("The number is odd");
+}
+    }
+}
